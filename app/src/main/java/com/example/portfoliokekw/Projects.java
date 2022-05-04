cbe2d44dad5ea45c733c1039bcfcfb54a6c73460
Ahaan25 <ahaan25092002@gmail.com>
2022-05-05 04:13:09 +0530

    ded

--- app/src/main/java/com/example/portfoliokekw/Projects.java
+++ app/src/main/java/com/example/portfoliokekw/Projects.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Projects extends AppCompatActivity {
 
@@ -20,53 +21,152 @@ public class Projects extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_projects);
+        Toast.makeText(Projects.this, "Press and hold for more information",Toast.LENGTH_LONG).show();
 
-        /*
-        rescu.setOnClickListener(new View.OnClickListener() {
+        rescu=findViewById(R.id.textView7);
+        rescu.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
-            public void onClick(View view) {
-                Uri uri=Uri.parse("http://nbtc.naco.gov.in/");
-                Intent intent=new Intent(Intent.ACTION_VIEW, uri);
-                startActivity(intent);
+            public boolean onLongClick(View view) {
+                AlertDialog.Builder builder=new AlertDialog.Builder(Projects.this);
+                builder.setCancelable(true);
+                builder.setTitle("Description");
+                builder.setMessage("This project is an all you can think of emergency app with customizable emergencies, custom messages and emergency contacts. \nAll just a click away");
+
+                builder.setNegativeButton("Back", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+
+                builder.setPositiveButton("Open GitHub repo", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Uri uri=Uri.parse("https://github.com/Ahaan25/RescU");
+                        Intent intent=new Intent(Intent.ACTION_VIEW, uri);
+                        startActivity(intent);
+                    }
+                });
+                builder.show();
+                return false;
             }
         });
 
-        bhelp.setOnClickListener(new View.OnClickListener() {
+        bhelp=findViewById(R.id.textView9);
+        bhelp.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
-            public void onClick(View view) {
-                Uri uri=Uri.parse("http://nbtc.naco.gov.in/");
-                Intent intent=new Intent(Intent.ACTION_VIEW, uri);
-                startActivity(intent);
+            public boolean onLongClick(View view) {
+                AlertDialog.Builder builder=new AlertDialog.Builder(Projects.this);
+                builder.setCancelable(true);
+                builder.setTitle("Description");
+                builder.setMessage("This project is a records management, booking with automated email service and location for nearest hospitals app.");
+
+                builder.setNegativeButton("Back", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+
+                builder.setPositiveButton("Open GitHub repo", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Uri uri=Uri.parse("https://github.com/Ahaan25/BloodDonation");
+                        Intent intent=new Intent(Intent.ACTION_VIEW, uri);
+                        startActivity(intent);
+                    }
+                });
+                builder.show();
+                return false;
             }
         });
 
-        guitarstrum.setOnClickListener(new View.OnClickListener() {
+        guitarstrum=findViewById(R.id.textView10);
+        guitarstrum.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
-            public void onClick(View view) {
-                Uri uri=Uri.parse("http://nbtc.naco.gov.in/");
-                Intent intent=new Intent(Intent.ACTION_VIEW, uri);
-                startActivity(intent);
+            public boolean onLongClick(View view) {
+                AlertDialog.Builder builder=new AlertDialog.Builder(Projects.this);
+                builder.setCancelable(true);
+                builder.setTitle("Description");
+                builder.setMessage("App made to do the job of a guitarist without actually needing a guitar or a guitarist! \nJust specify the chords and strumming pattern, et voila! It will play the music for you");
+
+                builder.setNegativeButton("Back", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+
+                builder.setPositiveButton("Open GitHub repo", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Uri uri=Uri.parse("https://github.com/Ahaan25/GuitarStrum");
+                        Intent intent=new Intent(Intent.ACTION_VIEW, uri);
+                        startActivity(intent);
+                    }
+                });
+                builder.show();
+                return false;
             }
         });
 
-        omnitrix.setOnClickListener(new View.OnClickListener() {
+        omnitrix=findViewById(R.id.textView11);
+        omnitrix.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
-            public void onClick(View view) {
-                Uri uri=Uri.parse("http://nbtc.naco.gov.in/");
-                Intent intent=new Intent(Intent.ACTION_VIEW, uri);
-                startActivity(intent);
+            public boolean onLongClick(View view) {
+                AlertDialog.Builder builder=new AlertDialog.Builder(Projects.this);
+                builder.setCancelable(true);
+                builder.setTitle("Description");
+                builder.setMessage("First project on android wear, created to mimic the omnitrix watch from the show Ben 10. \nIt creates custom watch faces with sound effects");
+
+                builder.setNegativeButton("Back", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+
+                builder.setPositiveButton("Open GitHub repo", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Uri uri=Uri.parse("https://github.com/Ahaan25/Omnitrix");
+                        Intent intent=new Intent(Intent.ACTION_VIEW, uri);
+                        startActivity(intent);
+                    }
+                });
+                builder.show();
+                return false;
             }
         });
 
-        anonforum.setOnClickListener(new View.OnClickListener() {
+        anonforum=findViewById(R.id.textView12);
+        anonforum.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
-            public void onClick(View view) {
-                Uri uri=Uri.parse("http://nbtc.naco.gov.in/");
-                Intent intent=new Intent(Intent.ACTION_VIEW, uri);
-                startActivity(intent);
+            public boolean onLongClick(View view) {
+                AlertDialog.Builder builder=new AlertDialog.Builder(Projects.this);
+                builder.setCancelable(true);
+                builder.setTitle("Description");
+                builder.setMessage("This project was made to be an anonymous forum page for the students of VITB to ask doubts and questions without restrictions. Obviously some content will be monitored.");
+
+                builder.setNegativeButton("Back", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+
+                builder.setPositiveButton("Open GitHub repo", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Uri uri=Uri.parse("https://github.com/Ahaan25/AnonForum");
+                        Intent intent=new Intent(Intent.ACTION_VIEW, uri);
+                        startActivity(intent);
+                    }
+                });
+                builder.show();
+                return false;
             }
         });
-        */
 
     }
 
